6314c95a6f6e1daa7321b2b04b6a3722de405962
LaToya Simon <1stforgottentoy@gmail.com>
2024-06-18 14:35:57 -0400

    last minute clean up

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,28 +1,26 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
-
+    
     state = {
         lightColor: "green",
     }
-
-        lightChange = () => {
-           switch(this.state.lightColor) {
-    case "green":
-        this.setState({lightColor: "yellow"});
-        break;
-    case "yellow":
+    
+    lightChange = () => {
+        switch(this.state.lightColor) {
+            case "green":
+                this.setState({lightColor: "yellow"});
+                break;
+            case "yellow":
                 this.setState({lightColor: "red"});
-
-        break;
-    case "red":
+                break;
+            case "red":
                 this.setState({lightColor: "green"});
-
-        break;
-    default:
-        console.log("Good job, you broke a stop light. -slow clap-");
-        break;
-    }
+                break;
+            default:
+                console.log("Good job, you broke a stop light. -slow clap-");
+                break;
+        }
     }
 
   render() {
@@ -30,10 +28,10 @@ export class ClassTrafficLight extends Component {
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className={`circle + ${this.state.lightColor === "red" ? "red" : "black"}`}></div> 
-        <div className={`circle + ${this.state.lightColor === "yellow" ? "yellow" : "black"}`}></div>
-        <div className={`circle + ${this.state.lightColor=== "green" ? "green" : "black"}`}></div>
+            {/* Background color can be black | yellow | red | green */}
+            <div className={`circle ${this.state.lightColor === "red" ? "red" : "black"}`}></div> 
+            <div className={`circle ${this.state.lightColor === "yellow" ? "yellow" : "black"}`}></div>
+            <div className={`circle ${this.state.lightColor=== "green" ? "green" : "black"}`}></div>
         </div>
         <button className="next-state-button" onClick={this.lightChange}>Next State</button>
       </div>
